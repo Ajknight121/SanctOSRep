ed5655eda71029d62cf6c1dad962f631de8294ef
Ajknight121 <aknig2@uic.edu>
2025-03-29 01:25:18 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,72 @@
+import "./App.css";
+
+import star from "./images/Star-black.svg";
+import logo from "./images/logo.svg";
+import logoColon from "./images/Logo-colon.svg";
+import logoTitle from "./images/LogoTitle.svg";
+import background from "./images/ui-background.svg";
+import cloudCreatures from "./images/cloud-creatures.png";
+import bbHeadshot from "./images/bb-headshot.png";
+import ccHeadshot from "./images/cc-headshot.png";
+
+function App() {
+  return (
+    <div className="page">
+      <div className="header col">
+        <div className="header-logo">
+          <img src={logoColon} width={"33%"}/>
+          <img src={logoTitle} width={"66%"} height={"120%"}/>
+          {/* <h1>DUSQK:</h1> */}
+        </div>
+
+        <div className="header-title-group col self-center">
+          {/* <h1>Sanctuary OS</h1> */}
+          <div className="header-route">
+            <h2>{"->"} menu</h2>
+          </div>
+        </div>
+      </div>
+      <div className="content row">
+        <div className="avatar-display"></div>
+        <div className="menu col">
+          <div className="sn-detail row inline">
+            SN. <div className="sn-value">123456789-Ajknight</div>
+            <div className="sn-label">Select*</div>
+          </div>
+          <ul className="nav-menu">
+            <li className="nav-option">GAMES</li>
+            <li className="nav-option">SETTINGS</li>
+            <li className="nav-option nav-option-ext">MEM.CARD</li>
+            <li className="nav-option">NOTEPAD</li>
+          <div className="nav-menu-description">
+            This is a description of the highlighted menu option
+          </div>
+          </ul>
+          <ul className="sub-menu">
+            <li className="nav-option">CREDITS</li>
+          </ul>
+        </div>
+        <div className="creatures">
+          <div className="creature-headshots row">
+            <div className="creature-headshot">
+              <img src={bbHeadshot}/>
+              <div className="creature-label">BB*</div>
+            </div>
+            <div className="creature-headshot">
+              <img src={ccHeadshot}/>
+              <label className="creature-label">CC*</label>
+            </div>
+          </div>
+          <div className="creature-large">
+            <img src={cloudCreatures} />
+          </div>
+        </div>
+      </div>
+      <div className="background">
+        <img className="background-svg" src={background} width={"90%"} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
