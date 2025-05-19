6a75a42d167e3fd56bf3937fd2872845d647bded
Ajknight121 <aknig2@uic.edu>
2025-05-19 11:29:05 -0500

    Credit discord, mobile section font size

--- src/components/Credits.tsx
+++ src/components/Credits.tsx
@@ -6,16 +6,20 @@ export default function Credits() {
   return (
     <div className='section credits'>
       <div className='section-header'>CREDITS</div>
-      <div className='section-container'>
+      <div className='section-container credits'>
         <div className='head'>Original design and music by:</div>
         <div><a href="https://www.youtube.com/@Dusqk" target="_blank">DUSQK</a></div>
         <div>Original Video:</div>
         <div className="sub"><a href="https://youtu.be/d3KfkKXRDzk">https://youtu.be/d3KfkKXRDzk</a></div>
-        <br/>
-        <div>Recreation by:</div>
+        
+        <p>Join the Official DUSQK discord <a href="https://discord.com/invite/sNrzkaAWmY" target='_blank'>HERE</a> to follow their official projects!</p>
+
+        <div>This fan recreation by:</div>
         <div><a href='https://github.com/Ajknight121/SanctOSRep' target='_blank'>@Ajknight</a></div>
-        This is a fan-made recreation for educational/fun purposes. I do not claim any ownership of the original concept.
+        This is a personal project I made for educational/fun purposes. I do not claim any ownership of the original concept.
       </div>
+      <div className="arrow-down"></div>
+      <div className="arrow-up"></div>
     </div>
   )
 }
